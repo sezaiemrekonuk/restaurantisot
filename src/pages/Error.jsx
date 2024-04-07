9ce8dbeb837f0c454e49f0a7c06d445d404f651e
Ufuk Yılmazlar <ufukyilmazlar@Ufuk-MacBook-Air.local>
2024-04-07 22:57:09 +0300

    Extra changes conducted

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -16,8 +16,8 @@ const Cancel = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-red-200">
       <div className="text-center p-8 bg-white rounded-md shadow-lg">
-        <h1 className="text-4xl font-bold text-red-800 mb-4">Payment Canceled</h1>
-        <p className="text-lg text-gray-700 mb-4">Your payment has been canceled.</p>
+        <h1 className="text-4xl font-bold text-red-800 mb-4">Payment Cancelled</h1>
+        <p className="text-lg text-gray-700 mb-4">Your payment has been cancelled.</p>
         <button
           onClick={handleClickButton}
           className="bg-red-800 text-white px-6 py-2 rounded-md hover:bg-red-600 focus:outline-none"
